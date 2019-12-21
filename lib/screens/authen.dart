4be891c0f12e1ef39ced67c670c884753219169a
ptpJ <patapong.apaipong@gmail.com>
2019-12-21 09:51:21 +0700

    Start 21Dec

---
+++ lib/screens/authen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:jotour/screens/register.dart';
+import 'package:jotour/utility/my_style.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+// Field
+
+// Method
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: MyStyle().textColor,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      highlightedBorderColor: MyStyle().textColor,
+      child: Text(
+        'Sign Up',
+        style: TextStyle(
+          color: MyStyle().textColor,
+        ),
+      ),
+      onPressed: () {
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext context) {
+              return Register();
+            });
+
+        Navigator.of(context).push(materialPageRoute);
+      },
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 8.0,
+        ),
+        signUpButton(),
+      ],
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          labelText: 'username :',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'password :',
+        ),
+      ),
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo1.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text('Jo tour',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: MyStyle().textColor,
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/camp.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(190, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(24)),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    showLogo(),
+                    showAppName(),
+                    userForm(),
+                    passwordForm(),
+                    showButton(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  
+// Field
+double h1 = 30.0, h2= 18.0;
+
+Color textColor = Colors.blueGrey[700];
+Color mainColor = Colors.blue;
+
+
+
+// Method
+MyStyle();
+
+}
